35a2d45d32a734469eaf8d1a19e3315d1e2eaf01
Daniel Masek <daniel@masci.cz>
2022-04-12 23:24:34 +0200

    Section 03 - Testing CSS

--- sec-03-basics/src/main/java/dmk/jfxamd/basics/App.java
+++ sec-03-basics/src/main/java/dmk/jfxamd/basics/App.java
@@ -2,11 +2,11 @@ package dmk.jfxamd.basics;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * JavaFX App
@@ -17,19 +17,17 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("primary.fxml"));
+        AnchorPane pane = fxmlLoader.load();
+        
+        scene = new Scene(pane);
+        
+        scene.getStylesheets().addAll(App.class.getResource("style.css").toExternalForm());
+        
         stage.setScene(scene);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
 
     public static void main(String[] args) {
         launch();
