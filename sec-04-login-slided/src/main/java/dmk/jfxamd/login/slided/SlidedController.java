24b9b4693b2906c881289a7a3bfc8f2b49700339
Daniel Masek <daniel@masci.cz>
2022-05-30 21:57:58 +0200

    login-slided: Add components

--- sec-04-login-slided/src/main/java/dmk/jfxamd/login/slided/SlidedController.java
+++ sec-04-login-slided/src/main/java/dmk/jfxamd/login/slided/SlidedController.java
@@ -2,7 +2,9 @@ package dmk.jfxamd.login.slided;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -11,12 +13,23 @@ import javafx.fxml.Initializable;
  */
 public class SlidedController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+  @FXML
+  private Pane slide1;
+
+  @FXML
+  private Pane slide2;
+
+  @FXML
+  private Pane slide3;
+
+  @FXML
+  private Pane slide4;
+
+  /**
+   * Initializes the controller class.
+   */
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    // TODO
+  }
 }
