961d450efd2251d17588bce5620ef84fe208629e
Daniel Masek <daniel@masci.cz>
2022-07-03 12:40:36 +0200

    splash-screen: Use TransitionPlayer where it is possible

--- utilities/src/main/java/dmk/jfxamd/utilities/transition/TransitionPlayer.java
+++ utilities/src/main/java/dmk/jfxamd/utilities/transition/TransitionPlayer.java
@@ -2,6 +2,7 @@ package dmk.jfxamd.utilities.transition;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import javafx.animation.Transition;
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,26 +10,60 @@ import lombok.extern.slf4j.Slf4j;
 public class TransitionPlayer {
 
   private final List<TransitionBuilder<?>> builderList = new ArrayList<>();
+  private Transition firstTransition;
+  private Transition lastTransition;
 
   public <T extends Transition> void add(TransitionBuilder<T> builder) {
-    log.info("Added builder: {}", builder);
+    log.debug("Added builder: {}", builder);
     builderList.add(builder);
   }
 
   public void play() {
-    log.info("Start play - size: {}", builderList.size());
+    log.debug("Start play - size: {}", builderList.size());
+    build();
+    getFirstTransition().ifPresent(Transition::play);
+  }
+
+  public void playInfinite() {
+    log.debug("Start play infinite - size: {}", builderList.size());
+    build();
+    if (getFirstTransition().isPresent() && getLastTransition().isPresent()) {
+      lastTransition.setOnFinished(actionEvent -> firstTransition.play());
+      firstTransition.play();
+    }
+  }
+
+  public Optional<Transition> getFirstTransition() {
+    return Optional.ofNullable(firstTransition);
+  }
+
+  public Optional<Transition> getLastTransition() {
+    return Optional.ofNullable(lastTransition);
+  }
+
+  private void build() {
     if (builderList.isEmpty()) {
       return;
     }
 
-    buildNext(builderList.get(0), 1).play();
+    build(builderList.get(0), 1);
   }
 
-  private Transition buildNext(TransitionBuilder<?> current, int nextIndex) {
+  private Transition build(TransitionBuilder<?> current, int nextIndex) {
     if (nextIndex < builderList.size()) {
-      var nextTransitionBuild = buildNext(builderList.get(nextIndex), nextIndex + 1);
+      var nextTransitionBuild = build(builderList.get(nextIndex), nextIndex + 1);
       current.withOnFinishedHandler(actionEvent -> nextTransitionBuild.play());
     }
-    return current.build();
+    var transition = current.build();
+    // set last transition
+    if (nextIndex == builderList.size()) {
+      lastTransition = transition;
+    }
+    // set first transition
+    if (nextIndex == 1) {
+      firstTransition = transition;
+    }
+    // return transition
+    return transition;
   }
 }
