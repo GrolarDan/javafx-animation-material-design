af910244974fab518a776fb15144d358fcb950db
Daniel Masek <daniel@masci.cz>
2022-07-09 22:30:13 +0200

    management-table: Implement button actions

--- sec-09-management-table/src/main/java/dmk/jfxamd/management/table/ManagementTableController.java
+++ sec-09-management-table/src/main/java/dmk/jfxamd/management/table/ManagementTableController.java
@@ -2,19 +2,22 @@ package dmk.jfxamd.management.table;
 
 import dmk.jfxamd.management.table.model.Person;
 import dmk.jfxamd.utilities.common.Utils;
+import io.github.palexdev.materialfx.controls.MFXComboBox;
 import io.github.palexdev.materialfx.controls.MFXTableColumn;
 import io.github.palexdev.materialfx.controls.MFXTableView;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.ResourceBundle;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import javafx.collections.FXCollections;
+import javafx.collections.MapChangeListener;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -25,6 +28,22 @@ public class ManagementTableController implements Initializable {
 
   @FXML
   private MFXTableView<Person> table;
+  @FXML
+  private Label ageLbl;
+  @FXML
+  private Label genderLbl;
+  @FXML
+  private Label jobLbl;
+  @FXML
+  private Label nameLbl;
+  @FXML
+  private MFXTextField ageTxt;
+  @FXML
+  private MFXComboBox<String> genderCmb;
+  @FXML
+  private MFXTextField jobTxt;
+  @FXML
+  private MFXTextField nameTxt;
 
   /**
    * Initializes the controller class.
@@ -36,7 +55,7 @@ public class ManagementTableController implements Initializable {
     MFXTableColumn<Person> ageColumn = new MFXTableColumn<>("AGE", true);
     MFXTableColumn<Person> genderColumn = new MFXTableColumn<>("GENDER", true);
 
-    double columnWidth = table.getPrefWidth()/4;
+    double columnWidth = table.getPrefWidth() / 4;
     nameColumn.setRowCellFactory(createTableRowCell(Person::getName));
     nameColumn.setPrefWidth(columnWidth);
     jobColumn.setRowCellFactory(createTableRowCell(Person::getJob));
@@ -48,6 +67,53 @@ public class ManagementTableController implements Initializable {
 
     table.getTableColumns().addAll(nameColumn, jobColumn, ageColumn, genderColumn);
     table.setItems(initData(10));
+    table.getSelectionModel().selectionProperty().addListener((MapChangeListener<? super Integer, ? super Person>) change -> {
+      if (change.wasAdded()) {
+        Person person = change.getValueAdded();
+        nameTxt.setText(person.getName());
+        nameLbl.setText(person.getName());
+        jobTxt.setText(person.getJob());
+        jobLbl.setText(person.getJob());
+        ageTxt.setText(person.getAge());
+        ageLbl.setText(person.getAge());
+        genderCmb.getSelectionModel().selectItem(person.getGender());
+        genderLbl.setText(person.getGender());
+      }
+    });
+    genderCmb.getItems().addAll("Male", "Female");
+  }
+
+  @FXML
+  void add(ActionEvent event) {
+    var person = new Person(nameTxt.getText(), jobTxt.getText(), ageTxt.getText(), genderCmb.getSelectedItem());
+    table.getItems().add(person);
+  }
+
+  @FXML
+  void clear(ActionEvent event) {
+    nameTxt.clear();
+    jobTxt.clear();
+    ageTxt.clear();
+    genderCmb.getSelectionModel().clearSelection();
+  }
+
+  @FXML
+  void delete(ActionEvent event) {
+    var personOpt = table.getSelectionModel().getSelectedValues().stream().findFirst();
+    personOpt.ifPresent(person -> table.getItems().remove(person));
+  }
+
+  @FXML
+  void edit(ActionEvent event) {
+    var selectedKey = table.getSelectionModel().getSelection().keySet().stream().findFirst();
+    selectedKey.ifPresent(key -> {
+      var person = table.getItems().get(key);
+      person.setName(nameTxt.getText());
+      person.setJob(jobTxt.getText());
+      person.setAge(ageTxt.getText());
+      person.setGender(genderCmb.getSelectedItem());
+      table.getCell(key).updateRow();
+    });
   }
 
   private ObservableList<Person> initData(int count) {
--- sec-09-management-table/src/main/java/dmk/jfxamd/management/table/model/Person.java
+++ sec-09-management-table/src/main/java/dmk/jfxamd/management/table/model/Person.java
@@ -1,13 +1,66 @@
 package dmk.jfxamd.management.table.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
-@Data
-@AllArgsConstructor
 public class Person {
-  private String name;
-  private String job;
-  private String age;
-  private String gender;
+  private StringProperty name;
+  private StringProperty job;
+  private StringProperty age;
+  private StringProperty gender;
+
+  public Person(String name, String job, String age, String gender) {
+    this.name = new SimpleStringProperty(name);
+    this.job = new SimpleStringProperty(job);
+    this.age = new SimpleStringProperty(age);
+    this.gender = new SimpleStringProperty(gender);
+  }
+
+  public String getName() {
+    return name.get();
+  }
+
+  public void setName(String name) {
+    this.name.set(name);
+  }
+
+  public String getJob() {
+    return job.get();
+  }
+
+  public void setJob(String job) {
+    this.job.set(job);
+  }
+
+  public String getAge() {
+    return age.get();
+  }
+
+  public void setAge(String age) {
+    this.age.set(age);
+  }
+
+  public String getGender() {
+    return gender.get();
+  }
+
+  public void setGender(String gender) {
+    this.gender.set(gender);
+  }
+
+  public StringProperty nameProperty() {
+    return name;
+  }
+
+  public StringProperty jobProperty() {
+    return job;
+  }
+
+  public StringProperty ageProperty() {
+    return age;
+  }
+
+  public StringProperty genderProperty() {
+    return gender;
+  }
 }
